3711ecfdec48cc13d6b92fe847b8f90762555ab2
Taiga Araki-McGuire <taraki0925@gmail.com>
2024-11-16 17:18:01 -0800

    Run Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: -> { Rails.env.production? }
 
-  private 
+  private
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
